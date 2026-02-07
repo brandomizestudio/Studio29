81f1b0bfe7ce0c4ce5b349abfe82992e91980d4a
brandomizestudio <brandomize.studio@gmail.com>
2026-02-07 13:39:50 +0530

    Fix some issue and change hero image

--- script.js
+++ script.js
@@ -337,15 +337,6 @@ document.addEventListener('DOMContentLoaded', () => {
              // Toggle Nav
              navLinks.classList.toggle('active');
              hamburger.classList.toggle('toggle');
- 
-             // Animate Links
-             links.forEach((link, index) => {
-                 if (link.style.animation) {
-                     link.style.animation = '';
-                 } else {
-                     link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-                 }
-             });
         };
 
         hamburger.addEventListener('click', toggleMenu);
